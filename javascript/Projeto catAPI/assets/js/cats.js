bdc6c10a30644884d6432d5c4adfdc677eb3b1ec
Kiskadee <Kiskadee.dev@gmail.com>
2021-12-02 00:44:54 -0300

    Projeto Cat API, onde se consome fotos de gatinhos provenientes de uma API rest

--- javascript/Projeto catAPI/assets/js/cats.js
+++ javascript/Projeto catAPI/assets/js/cats.js
@@ -2,7 +2,10 @@ const BASE_URL = 'https://thatcopy.pw/catapi/rest/'
 
 const catHtmlImg = document.getElementById("cat-pic");
 const changePicButton = document.getElementById("changeCatPicture");
+const loadingButton = document.getElementById("loadingButton");
 
+
+let current_status = 0; 
 
 const getCats = async () => {
     try{
@@ -14,8 +17,20 @@ const getCats = async () => {
     }
 }
 
+const changeButtonState = (isLoading) => {
+    if(isLoading){
+        changePicButton.style.display = "none";
+        loadingButton.style.display = "initial";
+    }else{
+        changePicButton.style.display = "initial";
+        loadingButton.style.display = "none";
+    }
+}
+
 const loadImg = async () => {
+    changeButtonState(true);
     catHtmlImg.src = await getCats();
+    changeButtonState(false);
 }
 
 changePicButton.addEventListener('click', loadImg);
