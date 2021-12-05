165a5318bdc50fede17c720fc65279312ea4c260
Kiskadee <Kiskadee.dev@gmail.com>
2021-12-05 00:10:47 -0300

    Parte 21

--- javascript/Projeto primeiro jogo de naves/assets/js/js.js
+++ javascript/Projeto primeiro jogo de naves/assets/js/js.js
@@ -363,12 +363,30 @@ function start() { // Inicio da função start()
             case 0:
                 $("#energia").css("background-image", "url(assets/imgs/energia0.png)");
                 //Game Over
+                gameOver();
                 break;
             default:
                 let err = new Error("Energia em um valor inválido!");
                 throw err;	
 	    } // Fim da função energia()
     }
-
+    //Função GAME OVER
+    function gameOver() {
+        fimdejogo=true;
+        musica.pause();
+        somGameover.play();
+        
+        window.clearInterval(jogo.timer);
+        jogo.timer=null;
+        
+        $("#jogador").remove();
+        $("#inimigo1").remove();
+        $("#inimigo2").remove();
+        $("#amigo").remove();
+        
+        $("#fundoGame").append("<div id='fim'></div>");
+        
+        $("#fim").html("<h1> Game Over </h1><p>Sua pontuação foi: " + pontos + "</p>" + "<div id='reinicia' onClick=reiniciaJogo()><h3>Jogar Novamente</h3></div>");
+    } // Fim da função gameOver();
 
 } // Fim start
