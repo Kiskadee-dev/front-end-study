f6b2370c473e7a3d87e61241dab80b2f3ed50ecd
Kiskadee <Kiskadee.dev@gmail.com>
2021-12-08 04:14:17 -0300

    Limita a execução do script a qualquer bootcamp da DIO

--- javascript/contar progresso do bootcamp/contar.js
+++ javascript/contar progresso do bootcamp/contar.js
@@ -1,76 +1,78 @@
-const EXTRA_INFO_DIV = document.getElementsByClassName("extra-info");
-const EXTRA_INFO_LIST = EXTRA_INFO_DIV[0].getElementsByTagName('ul')[0];
+if (window.location.href.includes('track')) {
+    const EXTRA_INFO_DIV = document.getElementsByClassName("extra-info");
+    const EXTRA_INFO_LIST = EXTRA_INFO_DIV[0].getElementsByTagName('ul')[0];
 
-let newButton = document.createElement('button');
-newButton.classList.add('btn');
-newButton.classList.add('btn-danger');
-newButton.classList.add('btn-block');
-newButton.innerHTML = "EXIBIR TAXA DE CONCLUSÃO DO BOOTCAMP";
+    let newButton = document.createElement('button');
+    newButton.classList.add('btn');
+    newButton.classList.add('btn-danger');
+    newButton.classList.add('btn-block');
+    newButton.innerHTML = "EXIBIR TAXA DE CONCLUSÃO DO BOOTCAMP";
 
-let newLi = document.createElement('li');
-EXTRA_INFO_LIST.appendChild(newLi);
-newLi.appendChild(newButton);
-
-
-newButton.addEventListener('click', ContarCursos);
+    let newLi = document.createElement('li');
+    EXTRA_INFO_LIST.appendChild(newLi);
+    newLi.appendChild(newButton);
 
 
-function ContarCursos() {
-    const ELEMENTOS_BOTOES = document.getElementsByClassName('sc-elJkPf');
-    let BOTOES = [...ELEMENTOS_BOTOES];
+    newButton.addEventListener('click', ContarCursos);
 
 
-    const COMPLETO_STR = 'BAIXAR CERTIFICADO';
-    const INCOMPLETO_STR = 'COMEÇAR AGORA';
-    const CONTINUAR_STR = 'CONTINUAR';
-    const DESAFIOS_COMPLETOS = 'VISUALIZAR SUBMISSÕES';
+    function ContarCursos() {
+        const ELEMENTOS_BOTOES = document.getElementsByClassName('sc-elJkPf');
+        let BOTOES = [...ELEMENTOS_BOTOES];
 
-    let iniciar = BOTOES.filter( (item) => {
-        return item.innerHTML.includes(INCOMPLETO_STR);
-    })
 
-    let cursosCompletos = BOTOES.filter( (item) => {
-        return item.innerHTML.includes(COMPLETO_STR);
-    })
+        const COMPLETO_STR = 'BAIXAR CERTIFICADO';
+        const INCOMPLETO_STR = 'COMEÇAR AGORA';
+        const CONTINUAR_STR = 'CONTINUAR';
+        const DESAFIOS_COMPLETOS = 'VISUALIZAR SUBMISSÕES';
 
-    let continuar = BOTOES.filter( (item) => {
-        return item.innerHTML.includes(CONTINUAR_STR);
-    })
+        let iniciar = BOTOES.filter( (item) => {
+            return item.innerHTML.includes(INCOMPLETO_STR);
+        })
 
-    let desafiosCompletos = BOTOES.filter( (item) => {
-        return item.innerHTML.includes(DESAFIOS_COMPLETOS);
-    })
+        let cursosCompletos = BOTOES.filter( (item) => {
+            return item.innerHTML.includes(COMPLETO_STR);
+        })
 
-    let total = iniciar.length+cursosCompletos.length+continuar.length+desafiosCompletos.length;
+        let continuar = BOTOES.filter( (item) => {
+            return item.innerHTML.includes(CONTINUAR_STR);
+        })
 
-    let str_arr = [
-        `Cursos a iniciar: ${iniciar.length}`,
-        `Cursos completos: ${cursosCompletos.length}`,
-        `Cursos a continuar: ${continuar.length}`,
-        `Desafios completos: ${desafiosCompletos.length}`,
-        `Total de cursos: ${total}`,
-        `Porcentagem concluída: ${computarTotal(total)}`
-    ];
+        let desafiosCompletos = BOTOES.filter( (item) => {
+            return item.innerHTML.includes(DESAFIOS_COMPLETOS);
+        })
 
-    let output_str = function() {
-        let str = str_arr[0];
-        for(let i = 1; i < str_arr.length; i++){
-            str += '\n'+str_arr[i];
+        let total = iniciar.length+cursosCompletos.length+continuar.length+desafiosCompletos.length;
+
+        let str_arr = [
+            `Cursos a iniciar: ${iniciar.length}`,
+            `Cursos completos: ${cursosCompletos.length}`,
+            `Cursos a continuar: ${continuar.length}`,
+            `Desafios completos: ${desafiosCompletos.length}`,
+            `Total de cursos: ${total}`,
+            `Porcentagem concluída: ${computarTotal(total)}`
+        ];
+
+        let output_str = function() {
+            let str = str_arr[0];
+            for(let i = 1; i < str_arr.length; i++){
+                str += '\n'+str_arr[i];
+            }
+            return str;
+        }();
+    
+        alert(output_str);
+        console.log('Cursos a iniciar: ', iniciar.length);
+        console.log('Cursos completos: ', cursosCompletos.length);
+        console.log('Cursos a continuar: ', continuar.length);
+
+        console.log('Desafios completos: ', desafiosCompletos.length);
+        console.log('Total de cursos: ', total);
+        console.log('Porcentagem concluída: ', computarTotal(total));
+
+        function computarTotal(total){
+            return (((cursosCompletos.length+desafiosCompletos.length)/total)*100).toFixed(2).toString()+"%";
         }
-        return str;
-    }();
- 
-    alert(output_str);
-    console.log('Cursos a iniciar: ', iniciar.length);
-    console.log('Cursos completos: ', cursosCompletos.length);
-    console.log('Cursos a continuar: ', continuar.length);
-
-    console.log('Desafios completos: ', desafiosCompletos.length);
-    console.log('Total de cursos: ', total);
-    console.log('Porcentagem concluída: ', computarTotal(total));
-
-    function computarTotal(total){
-        return (((cursosCompletos.length+desafiosCompletos.length)/total)*100).toFixed(2).toString()+"%";
-    }
-};
+    };
+}
 
